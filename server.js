85b59f1f4789b349a0392a0f161e2fa45e241c3c
Regina <reina5425@gmail.com>
2024-09-29 19:14:06 +0300

    query and status code

--- server.js
+++ server.js
@@ -15,13 +15,25 @@ let posts = [
 
 // Get all posts
 app.get('/api/posts', (req, res) => {
-	res.json(posts);
+	const limit = parseInt(req.query.limit);
+
+	if(!isNaN(limit) && limit > 0) {
+		return res.status(200).json(posts.slice(0, limit));
+	}
+
+	res.status(200).json(posts);
 })
 
 // get single post
 app.get('/api/posts/:id', (req, res) => {
 	const id = parseInt(req.params.id);
-	res.json(posts.filter(post => post.id === id));
+	const post = posts.find(post => post.id === id);
+
+	if(!post) {
+		return res.status(404).json({message: `A post with the id of ${id} was not found`})
+	}
+
+	res.status(200).json(post);
 })
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
