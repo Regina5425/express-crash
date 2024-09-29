0147cf41c5c59338c887cf464a8284994c81c0ff
Regina <reina5425@gmail.com>
2024-09-29 18:31:24 +0300

    json and req params

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const path = require('path');
-// const PORT = process.env.PORT;
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 const app = express();
 
-app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, 'public', 'index.html'))
+let posts = [
+	{id: 1, title: 'Post One'},
+	{id: 2, title: 'Post Two'},
+	{id: 3, title: 'Post Three'},
+]
+
+// setup static folder
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// Get all posts
+app.get('/api/posts', (req, res) => {
+	res.json(posts);
 })
 
-app.get('/about', (req, res) => {
-	res.sendFile(path.join(__dirname, 'public', 'about.html'))
+// get single post
+app.get('/api/posts/:id', (req, res) => {
+	const id = parseInt(req.params.id);
+	res.json(posts.filter(post => post.id === id));
 })
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
