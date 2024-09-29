828b5497a021d60e4eb18350372ea5d970dd0861
Regina <reina5425@gmail.com>
2024-09-29 19:57:54 +0300

    add middleware

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import path from 'path';
 import posts from './routes/posts.js';
+import logger from './middleware/logger.js';
+
 const PORT = process.env.PORT || 8000;
 
 const app = express();
@@ -8,6 +10,9 @@ const app = express();
 // body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// logger middleware
+app.use(logger);
 
 // setup static folder
 // app.use(express.static(path.join(__dirname, 'public')));
