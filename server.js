fcc13672be95e209ed09f7eb53d9d1e46ba67b89
Regina <reina5425@gmail.com>
2024-09-29 18:11:15 +0300

    init

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+// const PORT = process.env.PORT;
+const PORT = 8000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
+app.get('/about', (req, res) => {
+	res.sendFile(path.join(__dirname, 'public', 'about.html'))
+})
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
