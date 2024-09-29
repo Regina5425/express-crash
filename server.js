a5540040fde5e3bdaa5de5e12fce2eedec9b418e
Regina <reina5425@gmail.com>
2024-09-29 19:47:52 +0300

    crud

--- server.js
+++ server.js
@@ -1,39 +1,18 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
+import posts from './routes/posts.js';
 const PORT = process.env.PORT || 8000;
 
 const app = express();
 
-let posts = [
-	{id: 1, title: 'Post One'},
-	{id: 2, title: 'Post Two'},
-	{id: 3, title: 'Post Three'},
-]
+// body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // setup static folder
 // app.use(express.static(path.join(__dirname, 'public')));
 
-// Get all posts
-app.get('/api/posts', (req, res) => {
-	const limit = parseInt(req.query.limit);
-
-	if(!isNaN(limit) && limit > 0) {
-		return res.status(200).json(posts.slice(0, limit));
-	}
-
-	res.status(200).json(posts);
-})
-
-// get single post
-app.get('/api/posts/:id', (req, res) => {
-	const id = parseInt(req.params.id);
-	const post = posts.find(post => post.id === id);
-
-	if(!post) {
-		return res.status(404).json({message: `A post with the id of ${id} was not found`})
-	}
-
-	res.status(200).json(post);
-})
+// Routes
+app.use('/api/posts', posts);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
